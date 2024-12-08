718b0f60f47240b8f9e4f716a347d20692f16b1d
mschulist <mschulist2@gmail.com>
2024-12-08 11:17:05 -0800

    fix bug that apparently wasn't a bug

--- frontend-v2/src/app/api/login/route.ts
+++ frontend-v2/src/app/api/login/route.ts
@@ -3,12 +3,12 @@ import { NextRequest } from 'next/server'
 const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:8000'
 
 export async function POST(request: NextRequest) {
+  const body = await request.text()
   return fetch(`${SERVER_URL}/token`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
     },
-    body: request.body,
-    duplex: 'half',
+    body: body,
   })
 }
